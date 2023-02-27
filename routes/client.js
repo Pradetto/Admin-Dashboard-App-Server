6159ba640e1313f1e0fb56871f4bcfe2dd776a1a
Michael Pradetto <pradetto5@gmail.com>
2023-02-26 19:39:14 -0600

    transactions added

--- routes/client.js
+++ routes/client.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getProduct } from "../controllers/client.js";
+import {
+  getProduct,
+  getCustomers,
+  getTransactions,
+} from "../controllers/client.js";
 
 const router = express.Router();
 
 router.get("/products", getProduct);
+router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
 
 export default router;
